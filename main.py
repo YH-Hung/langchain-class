95e35f777d0a7ea4f20330413ea935e391bd18f5
yinghanhung <hle.infdy@gmail.com>
2026-06-14 13:36:38 +0800

    1st sample

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
 def main():
-    print("Hello from langchain-class!")
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        api_key="lm-studio",  # any non-empty string usually works
+        model="qwen/qwen3.5-9b"
+    )
+
+    result = llm.invoke([HumanMessage("who are you?")])
+    print(result.content)
+    
 
 
 if __name__ == "__main__":
