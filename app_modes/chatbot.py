efdc878597b93dc33443ca417b7d903d715ea385
yinghanhung <hle.infdy@gmail.com>
2026-06-16 23:20:53 +0800

    provide memory for chatbot

--- app_modes/chatbot.py
+++ app_modes/chatbot.py
@@ -1,14 +1,29 @@
 from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from llm import create_llm
 
 
 EXIT_COMMANDS = {"exit", "quit"}
 
 
+class ChatHistoryStore:
+    def __init__(self):
+        self._store: dict[str, BaseChatMessageHistory] = {}
+
+    def get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self._store:
+            self._store[session_id] = InMemoryChatMessageHistory()
+        return self._store[session_id]
+
 def run(args):
     prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="history"),
         ("human", "{content}")
     ])
 
@@ -16,6 +31,16 @@ def run(args):
     parser = StrOutputParser()
     
     chain = prompt | llm | parser
+
+    # LangChain had switch memory mechanism to LangGraph. 
+    # If you need memory, DONNOT use LangChain, USE LangGraph instead.
+    history_store = ChatHistoryStore()
+    bot = RunnableWithMessageHistory(
+        chain,
+        history_store.get_session_history,
+        input_messages_key="content",
+        history_messages_key="history"
+    )
 
     print("Chatbot mode. Type 'exit' or 'quit' to stop.")
 
@@ -25,7 +50,7 @@ def run(args):
             if user_input.lower() in EXIT_COMMANDS:
                 break
 
-            result = chain.invoke({"content": user_input})
+            result = bot.invoke({"content": user_input}, {"configurable": {"session_id": "curr_session"}})
             print(result)
         except EOFError:
             print()
